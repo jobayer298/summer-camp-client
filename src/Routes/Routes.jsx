ae2e3c057c26549f4ae8e38c9a963accc95ff5ee
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2023-06-07 10:50:13 +0600

    navbad added

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "../Main/Main";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+    ]
+  },
+]);
