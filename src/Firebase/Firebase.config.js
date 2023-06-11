84db47b32e6292cb79230acfff6dae56d22ebab7
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2023-06-11 19:32:18 +0600

    firebase config added

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBFW1uQJcMfisePSYwvzcDKejAzkYE29MU",
-  authDomain: "summer-school-548ce.firebaseapp.com",
-  projectId: "summer-school-548ce",
-  storageBucket: "summer-school-548ce.appspot.com",
-  messagingSenderId: "963096150025",
-  appId: "1:963096150025:web:94176b8fe4be1c0c0038d8",
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
